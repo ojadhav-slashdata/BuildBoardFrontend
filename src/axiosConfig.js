44a221ee00de4a258a4f6ada25ce58cd349fe1ed
BuildBoard Team <buildboard@hackathon.dev>
2026-04-01 16:27:36 +0400

    fix: point API baseURL to deployed backend

--- src/axiosConfig.js
+++ src/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: 'https://build-board-backend.vercel.app/api',
 });
 
 let getToken = () => null;
